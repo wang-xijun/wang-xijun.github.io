73a90aff3ed9977c1c18e247c04e8a9d38c83405
wang-xijun <xjwang22@gmail.com>
2018-09-18 21:29:48 +0800

    update

---
+++ runme.bash
@@ -0,0 +1,13 @@
+jemdoc index.jemdoc
+jemdoc group.jemdoc
+jemdoc list.jemdoc
+jemdoc DRL.jemdoc
+#python jemdoc.py home.jemdoc
+#python jemdoc.py education.jemdoc
+#python jemdoc.py research.jemdoc
+#python jemdoc.py publications.jemdoc
+#python jemdoc.py publicationstopic.jemdoc
+#python jemdoc.py teaching.jemdoc
+#python jemdoc.py group.jemdoc
+#python jemdoc.py talks.jemdoc
+#python jemdoc.py index.jemdoc
